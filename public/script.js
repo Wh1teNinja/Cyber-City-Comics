504b4bce05a1294c2c5237105de89409aac45697
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2021-06-29 12:44:52 -0400

    added replacement of '<' and '>' symbols to &lt; and &gt; for transcript

--- public/script.js
+++ public/script.js
@@ -16,7 +16,8 @@ const nextPage = () => {
 };
 
 const randomStrip = () => {
-  window.location.pathname = "/" + Math.floor(Math.random() * window.sessionStorage.getItem("lastId"));
+  window.location.pathname =
+    "/" + Math.floor(Math.random() * window.sessionStorage.getItem("lastId"));
 };
 
 window.onload = () => {
@@ -42,21 +43,25 @@ window.onload = () => {
       let comicNumber = document.getElementById("comic-number");
       comicNumber.innerHTML = comicId;
       let transcript = document.getElementById("transcript");
-      let transcriptContent = comicData.transcript.split('\n');
-      console.log(comicData.transcript);
-      transcript.innerHTML = transcriptContent.map((line) => {
-        if (line.startsWith("{{")) return `<p><b>${line.slice(2, -2)}</b></p>`
-        else if (line.startsWith("[[")) return `<p><i>${line.slice(2, -2)}</i></p>`
-        else return `<p>${line}</p>`}).filter(line => line !== "<p></p>").join("");
+      let transcriptContent = comicData.transcript.split("\n");
+      transcript.innerHTML = transcriptContent
+        .map((line) => {
+          if (line.startsWith("{{")) return `<p><b>${line.slice(2, -2)}</b></p>`;
+          else if (line.startsWith("[["))
+            return `<p><i>${line.slice(2, -2)}</i></p>`;
+          else return `<p>${line.replaceAll("<", "&lt;").replaceAll(">", "&gt;")}</p>`;
+        })
+        .filter((line) => line !== "<p></p>")
+        .join("");
     });
 
   if (!window.sessionStorage.getItem("lastId")) {
     fetch(window.location.origin + "/comic-data/last", {
       method: "GET",
     })
-    .then((res) => res.json())
-    .then((data) => {
-      window.sessionStorage.setItem("lastId", data.num);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        window.sessionStorage.setItem("lastId", data.num);
+      });
   }
 };
