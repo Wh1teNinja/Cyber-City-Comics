dd38b9daa4309a7ce2d138d0cbf724d369c1f0f6
Andrei Fedchenko <33954126+Wh1teNinja@users.noreply.github.com>
2021-06-29 13:17:45 -0400

    minor fixes and improvements

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const prevPage = () => {
   window.location.pathname = "/" + (parseInt(comicId) - 1);
 };
 const nextPage = () => {
-  if (comicId !== window.sessionStorage.getItem("lastId"))
+  if (comicId !== parseInt(window.sessionStorage.getItem("lastId")))
     window.location.pathname = "/" + (parseInt(comicId) + 1);
 };
 
@@ -15,10 +15,26 @@ const randomStrip = () => {
 };
 
 window.onload = () => {
+  if (!window.sessionStorage.getItem("lastId")) {
+    fetch(window.location.origin + "/comic-data/last", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        window.sessionStorage.setItem("lastId", data.num);
+      });
+  }
+
   comicId = window.location.pathname.split("/")[1];
-  let url = comicId
-    ? window.location.origin + "/comic-data/" + comicId
-    : window.location.origin + "/comic-data/last";
+
+  if (comicId >= parseInt(window.sessionStorage.getItem("lastId"))) {
+    window.location.pathname = "/";
+  }
+
+  let url =
+    comicId === ""
+      ? window.location.origin + "/comic-data/last"
+      : window.location.origin + "/comic-data/" + comicId;
   fetch(url, {
     method: "GET",
   })
@@ -43,19 +59,15 @@ window.onload = () => {
           if (line.startsWith("{{")) return `<p><b>${line.slice(2, -2)}</b></p>`;
           else if (line.startsWith("[["))
             return `<p><i>${line.slice(2, -2)}</i></p>`;
-          else return `<p>${line.replaceAll("<", "&lt;").replaceAll(">", "&gt;")}</p>`;
+          else
+            return `<p>${line.replaceAll("<", "&lt;").replaceAll(">", "&gt;")}</p>`;
         })
         .filter((line) => line !== "<p></p>")
         .join("");
-    });
 
-  if (!window.sessionStorage.getItem("lastId")) {
-    fetch(window.location.origin + "/comic-data/last", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        window.sessionStorage.setItem("lastId", data.num);
-      });
-  }
+      if (transcript.innerHTML !== "") {
+        let transcriptH2 = document.getElementById("transcript-h2");
+        transcriptH2.className = "";
+      }
+    });
 };
